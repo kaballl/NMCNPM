1a326dae412a543b60de3518a236dc27dd6f8f09
tienanhjkl <tienanhjkl8@gmail.com>
2022-01-01 17:22:19 +0700

    upload
    UI list my courses

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 
 const homeRouter=require('./home')
 const loginRouter=require('./login')
-
+const mycoursesRouter=require('./mycourses')
 
 function route(app){
    
+    app.use('/my-courses',mycoursesRouter)
     app.use('/home',homeRouter)
     app.use('/login',loginRouter)
 
